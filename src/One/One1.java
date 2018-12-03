707fb48b61dd6f032590eb440d673ff9e4c2f4ab
admin <admin@admin-PC>
2018-12-03 15:06:42 +0530

    ji

--- src/One/One1.java
+++ src/One/One1.java
@@ -1,5 +1,14 @@
 package One;
 
-public class One1 {
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class One1 {
+	static{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		
+	}
+	public static void main(String[] args) {
+		ChromeDriver driver= new ChromeDriver();
+		driver.close();
+	}
 }
